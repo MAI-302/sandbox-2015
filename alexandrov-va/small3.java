bf1db93d82c8e940ef88e1accddf4478c8369ed6
Vladimir <alexandrov.vov@gmail.com>
2015-03-01 19:36:20 +0200

    Update small3.java
    Задание №3

--- alexandrov-va/small3.java
+++ alexandrov-va/small3.java
@@ -5,14 +5,12 @@ public class small3
 
 	public static void main(String[] args) 
 	{
-		boolean isScalar1 = true;		//Первое условие скалярной матрицы
-		boolean isScalar2 = true;		//Второе условие скалярной матрицы
 		
 		int arr[][] = 
 			{
 				{4, 0, 0},
-				{0, 5, 0},
-				{1, 0, 4}
+				{0, 4, 0},
+				{0, 0, 4}
 			};
 		
 		//Вывод матрицы
@@ -25,41 +23,20 @@ public class small3
 			System.out.println("");
 		}
 		
-		//Проверка первого условия - все элементы не на главной диагонали нулевые
+		//Проверяем, скалярная ли матрица
 		for(int i = 0; i < arr.length; i++)
 		{
 			for(int j = 0; j < arr.length; j++)
 			{
-				if(i != j && arr[i][j] != 0 && isScalar1)
+				if((i != j && arr[i][j] != 0) || (i == j && (i+j)>1 && arr[i][j] != arr[i-1][j-1]))
 				{
-					System.out.println("Найден ненулевой элемент, находящийся не на главной диагонали.");
-					isScalar1 = false;
-				}
-			}
-		}
-
-		//Проверка второго условия - все элементы на главной диагонали одинаковые
-		for(int i = 1; i < arr.length; i++)
-		{
-			for(int j = 1; j < arr.length; j++)
-			{
-				if(i == j && arr[i][j] != arr[i-1][j-1] && isScalar2)
-				{
-					System.out.println("На главной диагонали элементы различаются.");
-					isScalar2 = false;
+					System.out.println("Матрица не скалярная.");
+					System.exit(0);
 				}
 			}
 		}
 		
-		//Вывод
-		if(isScalar1 && isScalar2)
-		{
-			System.out.println("Матрица скалярная.");
-		}
-		else
-		{
-			System.out.println("Матрица не является скалярной");
-		}
+		System.out.println("Матрица скалярная.");
 	}
 
 }
