04aa8e83a36c4b0cea4ce5b35515a644a771b8e0
Yaroslav Rodnin <begotten63@gmail.com>
2015-03-02 04:01:11 +0300

    upd task7

--- Rodnin/small_tasks/task7.cs
+++ Rodnin/small_tasks/task7.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace small_7
 {
-    class Program
+    class small7
     {
         static void Main(string[] args)
         {
@@ -17,42 +17,52 @@ namespace small_7
 
             int[,] mass = new int[n, m];
             Random rand = new Random();
+            int I, J;
             Console.WriteLine();
 
             Console.WriteLine("Матрица заполненная случайными числами в диапазоне от -100 до 100");
-            for (int i = 0; i < mass.GetLength(0); i++)
-            {
-                for (int j = 0; j < mass.GetLength(1); j++)
-                {
-                    mass[i, j] = rand.Next(-100, 100);
-                    Console.Write(mass[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-            int[] maxOfRows = new int[m];
-            int min;
 
-            maxOfRows[0] = mass[0, 0];
+            I = 0;
+		    while(I < mass.GetLength(0))
+		    {
+			    J = 0;
+			    while(J < mass.GetLength(1))
+			    {
+				        mass[I, J] = rand.Next(-100, 100);
+                        Console.Write(mass[I, J] + " ");
+				        J++;
+			    }
+			    Console.WriteLine();
+			    I++;
+		    }
 
-            for (int i = 0; i < mass.GetLength(0); i++)
-            {
-                for (int j = 0; j < mass.GetLength(1); j++)
-                {
-                    if (mass[i, j] > maxOfRows[i])
-                    {
-                        maxOfRows[i] = mass[i,j];
-                    }
-                }
-            }
+            
+            int MaxOfStr = 0;				//Индекс максимального элемента в строке
+            int minmax = int.MaxValue;		//Минимакс
+		    
+		
+		    
+		    I = 0;
+		    while(I < mass.GetLength(0))
+		    {
+			    J = 0;
+			    while(J < mass.GetLength(1))
+			    {
+			        if (mass[I, J] > mass[I, MaxOfStr]) 
+			        {
+			            MaxOfStr = J;
+			        }
+				    J++;
+			    }
 
-            min = maxOfRows[0];
+		        if (mass[I, MaxOfStr] < minmax) 
+		        {
+		            minmax = mass[I, MaxOfStr];
+		        }									
+			    I++;
+		    }
 
-            for (int i = 1; i < m; i++)
-            {
-                if (maxOfRows[i] < min)
-                    min = maxOfRows[i];
-            }
-            Console.WriteLine("Минимальный среди максимальных: " + Convert.ToString(min));
+            Console.WriteLine("Минимальный среди максимальных : " + minmax);
             Console.ReadKey();
         }
     }
