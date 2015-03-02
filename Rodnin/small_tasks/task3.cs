44bc26dd0922bf824f56517f38a493267713cf10
Yaroslav Rodnin <begotten63@gmail.com>
2015-03-02 14:17:19 +0300

    upd task3

--- Rodnin/small_tasks/task3.cs
+++ Rodnin/small_tasks/task3.cs
@@ -15,9 +15,6 @@ namespace small_3
             Console.WriteLine("Введите размерность матрицы n:");
             int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите число :");
-            int number = Convert.ToInt32(Console.ReadLine());
-
             int[,] matrix = new int[n, n];
             Random rand = new Random();
             Console.WriteLine();
@@ -30,34 +27,36 @@ namespace small_3
                 {
                     matrix[i, j] = rand.Next(-100, 100);
                     Console.Write(matrix[i, j] + " ");
-                    
+
                 }
                 Console.WriteLine();
             }
 
+            int number = matrix[0, 0];
 
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
+                if (matrix[i, i] != number)
+                {
+                    ifMatrixIsNumberMultUnMatrix = false;
+                }
+
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if ((i != j) && (matrix[i, j] != 0))
                     {
                         ifMatrixIsNumberMultUnMatrix = false;
-                    }      
+                    }
                 }
-                if (matrix[i, i] != number)
-                {
-                    ifMatrixIsNumberMultUnMatrix = false;
-                } 
             }
 
             if (ifMatrixIsNumberMultUnMatrix)
             {
-                Console.WriteLine("Матрица является произведением числа и единичной матрицы");
+                Console.WriteLine("Матрица является произведением числа " + number + " и единичной матрицы");
             }
             else
             {
-                Console.WriteLine("Матрица не является произведением числа и единичной матрицы"); 
+                Console.WriteLine("Матрица не является произведением числа и единичной матрицы");
             }
             Console.ReadKey();
         }
