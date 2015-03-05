333c7f96f78ac90479b9834179ab01c9f7175343
Vladimir <alexandrov.vov@gmail.com>
2015-03-05 14:41:43 +0300

    Ruby small4.rb
    Задание №4

---
+++ alexandrov-va/Ruby/small4.rb
@@ -0,0 +1,9 @@
+print "Введите слово: "
+word = gets
+
+arr = Array.new(word.length - 1){ |i| word[i]}							#массив из символов слова
+
+arr = arr.uniq.map{ |ch| [ch, arr.find_all{ |let| let == ch }.size]}	#создание хеша, где ключ - символ, значение - их количество в слове
+
+puts "[буква, число повторений в слове]:"
+p arr
