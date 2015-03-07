43aec1bf985d768c24344aabdef0837bff1d854f
Vladimir <alexandrov.vov@gmail.com>
2015-03-07 14:59:49 +0300

    Update Ruby small3.rb
    Добавил вывод исходной матрицы

--- alexandrov-va/Ruby/small3.rb
+++ alexandrov-va/Ruby/small3.rb
@@ -2,5 +2,7 @@ require 'matrix'
 
 arr = [[4, 0, 0, 0], [0, 4, 0, 0], [0, 0, 4, 0], [0, 0, 0, 4]]
 
+0.upto(arr.size - 1) { |i| p arr[i] }
+
 print "Матрица скалярная? - "
 puts Matrix.scalar(arr.size, arr[0][0]) == Matrix[*arr]
