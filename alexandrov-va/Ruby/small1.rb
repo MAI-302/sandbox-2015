fe1c07dbd6c9212e4d1a14f87645242cf6b396cc
Vladimir <alexandrov.vov@gmail.com>
2015-03-07 15:57:56 +0300

    Ruby small1.rb

---
+++ alexandrov-va/Ruby/small1.rb
@@ -0,0 +1,13 @@
+print "Введите слово: "
+word = gets
+
+count = (2...word.length).to_a.inject(1){ |result, i| result*i}
+
+arr = Array.new(word.length - 1){ |i| word[i]}
+
+arr = arr.uniq.map{ |ch| word.scan(ch).size }
+
+0.upto(arr.size - 1) { |i| count = count / (1..arr[i].to_i).to_a.inject(1){ |result, i| result*i}}
+
+print "Количество анаграмм: "
+puts count
