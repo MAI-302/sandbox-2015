9151c942a04956bcf7756ba884ad0a2669a72df5
Vladimir <alexandrov.vov@gmail.com>
2015-03-07 15:41:19 +0300

    Update Ruby small4.rb
    Упростил создание хеша.

--- alexandrov-va/Ruby/small4.rb
+++ alexandrov-va/Ruby/small4.rb
@@ -3,7 +3,7 @@ word = gets
 
 arr = Array.new(word.length - 1){ |i| word[i]}							#массив из символов слова
 
-arr = arr.uniq.map{ |ch| [ch, arr.find_all{ |let| let == ch }.size]}	#создание хеша, где ключ - символ, значение - их количество в слове
+arr = arr.uniq.map{ |ch| [ch, word.scan(ch).size]}	#создание хеша, где ключ - символ, значение - их количество в слове
 
 puts "[буква, число повторений в слове]:"
 p arr
