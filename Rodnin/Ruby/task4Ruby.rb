d845f4d8757ff4549bbd2e0611a38674734adee1
Yaroslav Rodnin <begotten63@gmail.com>
2015-03-07 14:11:23 +0300

    ruby task4

---
+++ Rodnin/Ruby/task4Ruby.rb
@@ -0,0 +1,6 @@
+print "Введите строку: "
+input = gets
+chars = Array.new(input.length - 1){ |i| input[i]} #массив символов содержащихся в строке
+chars = chars.uniq.collect{ |ch| [ch, chars.find_all { |key| key == ch }.size]} #Удаляем повторяющиеся,хешируем ключ-символ, значение - кол-во повторов
+puts "[Символ, число повторений в строке]:"
+p chars
