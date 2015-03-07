8f9272a7ddefea959bf04dcf8740d287bd9a1f3d
Igor Prokopenya <igorprokopenya@gmail.com>
2015-03-07 13:54:34 +0300

    task1.java
    СПАСИБО!

--- VladislavSakhno/task1.java
+++ VladislavSakhno/task1.java
@@ -9,7 +9,7 @@ public class task1 {
 	
 
 	
-	public static long fac( int n )
+	public static long fac( int n )		
 
     {
         if( n <= 1 )     // base case
@@ -23,17 +23,16 @@ public class task1 {
 	public static void main(String[] args) throws IOException{
 		int n;
 		Scanner br = new Scanner(System.in);
-		System.out.println("âåäèòå ñëîâî");
+		System.out.println("Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã±Ã«Ã®Ã¢Ã®");
 		String str = br.nextLine();
 		
-		 n = 1;
-		for(int i = 1; i < str.length()+1; i++){
-		n *= i;
-			
-		}
-		
+		 n = 1;					     // _$$$$__$$___$$_$$$$$$_$$__$$__$$$$_____$$$__$$$$__$$__$$_$$__$$__$$$$
+		for(int i = 1; i < str.length()+1; i++){     // $$__$$_$$$_$$$_$$_____$$_$$__$$__$$___$_$$_$$__$$_$$__$$_$$_$$__$$__$$
+		n *= i;					     //	$$_____$$_$_$$_$$$$___$$$$___$$$$$$__$__$$_$$__$$__$$$$$_$$$$___$$$$$$
+		}					     //	$$__$$_$$___$$_$$_____$$_$$__$$__$$_$$__$$_$$__$$_____$$_$$_$$__$$__$$
+							     //	_$$$$__$$___$$_$$$$$$_$$__$$_$$__$$_$$__$$__$$$$______$$_$$__$$_$$__$$
 	long k = fac(str.length());
-		System.out.println("×èñëî àíàãðàì : "+  n+ " ÷åðåç ôóíêöèþ :" + k);
+		System.out.println("Ã—Ã¨Ã±Ã«Ã® Ã Ã­Ã Ã£Ã°Ã Ã¬ : "+  n+ " Ã·Ã¥Ã°Ã¥Ã§ Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¾ :" + k);
 		
 	}
 	
