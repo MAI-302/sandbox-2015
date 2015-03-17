b48acf29132ca91b533be58315cd7eb09106d05d
Vladimir <alexandrov.vov@gmail.com>
2015-03-18 00:12:29 +0300

    Update small2.java
    Теперь с использованием одной переменной для подсчета скобок вместо стеков.

--- alexandrov-va/small2.java
+++ alexandrov-va/small2.java
@@ -1,52 +1,43 @@
-package small2;
+	package small2;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class small2 {
-
+	
+	static int counter;
+ 
+    public static boolean validate(String input) 
+    {
+        for (char c : input.toCharArray()) 
+        {
+            if (c == ')' && counter == 0) 
+            {
+                return false;
+            }
+            if (c == '(') 
+            {
+                counter++;
+            }
+            if (counter > 0 && c == ')') 
+            {
+                    counter--;
+            }
+        }
+        return (counter == 0);
+    }
+	
 	public static void main(String[] args) 
 	{
 		
-		String str;								//Слово
+		String str;
 		Scanner input = new Scanner(System.in);	
+		counter = 0;
 		
 		str = input.next();
-		char let;								//i-ый символ в слове
 		
-		int count = 0;								//Счетчик разности кол-ва левых и правых скобок
-		
-		int i = 0;
-		
-		do
-		{
-			let = str.toCharArray()[i];
-			
-			//Подсчет числа левых скобок и правых скобок
-			if(let == '(')
-				count++;
-			if(let == ')')
-				count--;
-			
-			//Проверка нахождения правой скобки после левой
-			if(let == '(' && str.indexOf(')', i) == -1)
-			{
-				System.out.println("Скобки расставлены неправильно.");
-				System.exit(0);
-			}
-			
-			i++;
-		}
-		while(i < str.length());
-		
-		//Проверка числа левых скобок и правых скобок
-		if(count == 0)
-		{
-			System.out.println("Скобки расставлены правильно.");
-		}
-		else
-		{
-			System.out.println("Скобки расставлены неправильно.");
-		}
+		boolean correct = validate(str);
+		System.out.println("Скобки расставлены " + (correct ? "" : "не") + "правильно");
 	}
 
 }
