75b0401f7d5e30c1a5847516026dc222538fb526
Yaroslav Rodnin <begotten63@gmail.com>
2015-03-18 01:32:52 +0300

    upd path task

--- Rodnin/Ruby/Path/path.rb
+++ Rodnin/Ruby/Path/path.rb
@@ -6,6 +6,12 @@ input.split("\n").each do |str|
     i += 1
 end
 
+i = j = input_array.count - 1
+while i != 0 || j != 0 do
+	input_array[i-1][j-1] = input_array[i-1][j-1] + [input_array[i][j], input_array[i][j-1]].max unless j.zero?
+	j = j.zero? ? i = i - 1 : j - 1
+end
+
 path = [0]
 level = 1
     path << input_array[level].index(input_array[level].max)
@@ -15,14 +21,8 @@ while level < input_array.count do
       level += 1
 end
 
-maxSum = input_array[0][0]
-(1...input_array.count.to_i).each do |i|
-	maxSum += input_array[i][path[i]]
-end
 
 print "Максимальная сумма по оптимальному пути: "
-puts maxSum
+puts input_array[0][0]
 puts "Индексы оптимального пути:"
 p path
-
-
